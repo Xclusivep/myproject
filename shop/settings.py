a846e825c94eb5ad3021524342a06e2ba10e9be4
Xclusivep <110981085+Xclusivep@users.noreply.github.com>
2025-02-09 21:58:27 +0100

    Update settings.py
    change ALLOWED_HOST to allow all

--- shop/settings.py
+++ shop/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-=uhetjuy+o^ek=+tr1agb-wl4^@j4_i=p7xk=!_=9zkxg2tx&4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
